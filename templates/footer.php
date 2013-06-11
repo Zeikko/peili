8e59cfb8ecbf4a10ea0c8b38100521d1a0590dc4
Jaakko Ojalehto <>
2013-06-11 22:54:42 +0300

    added y-tunnus

--- templates/footer.php
+++ templates/footer.php
@@ -5,6 +5,7 @@
         <h3 itemprop="name">Markkinointitoimisto Peili Oy</h3>
         <div>Puh: <span itemprop="telephone">044 544 5524</span></div>
         <div>Sähköposti: <span itemprop="email">asiakaspalvelu@peili-markkinointi.fi</span></div>
+        <div>Y-tunnus: 2545407-6</div>
     </div>
 </footer>
 
